44c242cbcec05b81382c0233ddd04a6ac466b8fc
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-01-06 16:55:12 +0800

    update todo sample

--- todoApp/clients/javascript/samples/typescript/src/todoItemSamples.ts
+++ todoApp/clients/javascript/samples/typescript/src/todoItemSamples.ts
@@ -13,24 +13,19 @@ const client = new TodoClient(endpoint,
 });
 
 async function main() {
-  const item: TodoItem = {
+  const item: any = {
     title: "Buy milk",
-    createdBy: 1,
     assignedTo: 10,
     description: "Need to buy milk",
     status: "InProgress",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    id: 1
   };
 
   // create a user
   const user = await client.users.create({
     username: "Harry Potter",
     email: "harry@email.com",
-    password: "password",
-    id: 0,
-  });
+    password: "password"
+  } as any);
   console.log(user);
 
   // get a non-exist todo item
