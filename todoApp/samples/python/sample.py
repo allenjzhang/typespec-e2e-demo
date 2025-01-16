675f6731b05f51847f9eaf98c7429e8b4a6f045b
Yuchao Yan <yuchaoyan@microsoft.com>
2025-01-16 15:37:21 +0800

    update python sample for todoapp

--- todoApp/samples/python/sample.py
+++ todoApp/samples/python/sample.py
@@ -6,39 +6,40 @@ from todo.models import (
     TodoItem,
     TodoAttachment,
     ToDoItemMultipartRequest,
+    FileAttachmentMultipartRequest,
 )
 from todo.todoitems.models import TodoItemPatch
 import os
 
-todoClient = TodoClient("http://localhost:5244", ServiceKeyCredential("dummy"))
+todo_client = TodoClient("http://localhost:5244", ServiceKeyCredential("dummy"))
 
 # create a user
-user = todoClient.users.create(User(username="John Doe", email="test@example.com", password="p@ssw0rd"))
+user = todo_client.users.create(User(username="John Doe", email="test@example.com", password="p@ssw0rd"))
 print(f"created user: {user}")
 
 # try to get a non-existent todo item
 try:
-    todo_item = todoClient.todo_items.get(0)
+    todo_item = todo_client.todo_items.get(0)
 except ResourceNotFoundError as exc_info:
     error = exc_info.response.json()
     print(f"Cannot find todo item: {error}")
 
 # create a todo item with json
-todo_item = todoClient.todo_items.create_json(
+todo_item = todo_client.todo_items.create_json(
     item=TodoItem(
         title="Buy milk",
         status="InProgress",
         assigned_to=10,
         description="Need to buy milk",
     ),
-    attachments=[TodoAttachment(filename="test.jpg", media_type="image/jpeg", contents=b"test")],
+    attachments=[TodoAttachment(filename="test1.jpg", media_type="image/jpeg", contents=b"test")],
 )
 print(f"Todo item {todo_item.id} created")
 
 # # create a todo item with multi part
 current_file_path = os.path.dirname(__file__)
 image_path = os.path.join(current_file_path, "image.jpg")
-todo_item = todoClient.todo_items.create_form(
+todo_item = todo_client.todo_items.create_form(
     ToDoItemMultipartRequest(
         item=TodoItem(title="Feed pet", status="InProgress", assigned_to=10, description="Need to feed pet"),
         attachments=[open(image_path, "rb")],
@@ -47,13 +48,13 @@ todo_item = todoClient.todo_items.create_form(
 print(f"Todo item {todo_item.id} created")
 
 # get the todo item
-retrieved_item = todoClient.todo_items.get(todo_item.id)
+retrieved_item = todo_client.todo_items.get(todo_item.id)
 print(
     f"Todo item {retrieved_item.id} retrieved, title: {retrieved_item.title}, status: {retrieved_item.status}, assigned_to: {retrieved_item.assigned_to}, description: {retrieved_item.description}"
 )
 
 # update the todo item
-updated_item = todoClient.todo_items.update(
+updated_item = todo_client.todo_items.update(
     todo_item.id,
     TodoItemPatch(title="Buy milk and bread", status="Completed", assigned_to=20),
 )
@@ -62,10 +63,24 @@ print(
 )
 
 # list all available todo items
-todo_items = todoClient.todo_items.list()
+todo_items = todo_client.todo_items.list()
 for item in todo_items:
     print(f"Item title: {item.title}, status: {item.status}")
 
+# upload a local file as the attachment of the created todo item
+todo_client.todo_items.attachments.create_json_attachment(
+    todo_item.id, TodoAttachment(filename="test2.jpg", media_type="image/jpeg", contents=b"test")
+)
+todo_client.todo_items.attachments.create_file_attachment(
+    todo_item.id, FileAttachmentMultipartRequest(contents=open(image_path, "rb"))
+)
+
+#  list all existing attachments
+pager = todo_client.todo_items.attachments.list(todo_item.id)
+for idx, attachment in enumerate(pager):
+    print(f"Attachment {idx} filename  : {attachment.filename}")
+    print(f"Attachment {idx} media-type: {attachment.media_type}")
+
 # delete the todo item
-todoClient.todo_items.delete(todo_item.id)
+todo_client.todo_items.delete(todo_item.id)
 print("Item deleted")
