fda989f906ee052e0a46d2eebb7f4b750f2f30d0
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-01-17 15:05:47 +0800

    update todo sample

--- todoApp/samples/javascript/todoItemSamples.js
+++ todoApp/samples/javascript/todoItemSamples.js
@@ -3,7 +3,7 @@ const { TodoClient } = require("@notabrand/todo-non-branded");
 // Load the .env file if it exists
 require("dotenv").config();
 
-const endpoint = process.env["ENDPOINT"] || "<endpoint>";
+const endpoint = process.env["ENDPOINT"] || "http://localhost:5244";
 
 const client = new TodoClient(
   endpoint,
@@ -56,6 +56,22 @@ async function main() {
     result.push(...item);
   }
   console.log(result);
+
+  // - create a new todo item via multipart/form-data operation
+  const createdTodoItem2 = await client.todoItems.attachments.createFileAttachment(1, {
+    contents:{ filename:"note1.txt", contentType:"application/octet-stream", contents:Buffer.from("./note1.txt")} }
+  );
+  console.log(createdTodoItem2);
+
+  // - upload a local file as the attachment of the created todo item
+  const attachment = await client.todoItems.attachments.createJsonAttachment(2, { filename:"note2.txt", mediaType:"application/json", contents:Buffer.from("./note2.txt") });
+  console.log(attachment);
+
+  // - list all existing attachments
+  const attachments = await client.todoItems.attachments.list(2);
+  for await (const attachment of attachments) {
+    console.log(attachment);
+  }
 
   // - delete the todo item
   const deleteTodoItem = await client.todoItems.delete(createdTodoItem.id);
