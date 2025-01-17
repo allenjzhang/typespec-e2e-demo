eb54ac9f9ee6dbeb9036fae447aef83b804caf25
Yuchao Yan <yuchaoyan@microsoft.com>
2025-01-17 10:09:47 +0800

    update sample.py for todo

--- todoApp/samples/python/sample.py
+++ todoApp/samples/python/sample.py
@@ -36,17 +36,6 @@ todo_item = todo_client.todo_items.create_json(
 )
 print(f"Todo item {todo_item.id} created")
 
-# # create a todo item with multi part
-current_file_path = os.path.dirname(__file__)
-image_path = os.path.join(current_file_path, "image.jpg")
-todo_item = todo_client.todo_items.create_form(
-    ToDoItemMultipartRequest(
-        item=TodoItem(title="Feed pet", status="InProgress", assigned_to=10, description="Need to feed pet"),
-        attachments=[open(image_path, "rb")],
-    )
-)
-print(f"Todo item {todo_item.id} created")
-
 # get the todo item
 retrieved_item = todo_client.todo_items.get(todo_item.id)
 print(
@@ -67,12 +56,29 @@ todo_items = todo_client.todo_items.list()
 for item in todo_items:
     print(f"Item title: {item.title}, status: {item.status}")
 
-# upload a local file as the attachment of the created todo item
-todo_client.todo_items.attachments.create_json_attachment(
-    todo_item.id, TodoAttachment(filename="test2.jpg", media_type="image/jpeg", contents=b"test")
+#  create a new todo item via multipart/form-data operation
+current_file_path = os.path.dirname(__file__)
+image_path = os.path.join(current_file_path, "image.jpg")
+todo_item = todo_client.todo_items.create_form(
+    ToDoItemMultipartRequest(
+        item=TodoItem(title="Feed pet", status="InProgress", assigned_to=10, description="Need to feed pet"),
+        attachments=[open(image_path, "rb")],
+    )
 )
+print(f"Todo item {todo_item.id} created")
+
+# upload a local file as the attachment of the created todo item
+## create attachment with json
+note1_path = os.path.join(current_file_path, "note1.txt")
+todo_client.todo_items.attachments.create_json_attachment(
+    todo_item.id,
+    TodoAttachment(filename="note1.txt", media_type="application/octet-stream", contents=open(note1_path, "rb").read()),
+)
+
+## create attachment with multipart/form-data
+note2_path = os.path.join(current_file_path, "note2.txt")
 todo_client.todo_items.attachments.create_file_attachment(
-    todo_item.id, FileAttachmentMultipartRequest(contents=open(image_path, "rb"))
+    todo_item.id, FileAttachmentMultipartRequest(contents=open(note2_path, "rb"))
 )
 
 #  list all existing attachments
@@ -80,6 +86,7 @@ pager = todo_client.todo_items.attachments.list(todo_item.id)
 for idx, attachment in enumerate(pager):
     print(f"Attachment {idx} filename  : {attachment.filename}")
     print(f"Attachment {idx} media-type: {attachment.media_type}")
+    print(f"Attachment {idx} contents: {attachment.contents}")
 
 # delete the todo item
 todo_client.todo_items.delete(todo_item.id)
